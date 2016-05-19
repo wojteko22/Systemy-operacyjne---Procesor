39a49b0d6b814828b08834a617fde8529419aff3
wojteko22 <wojteko22.96@gmail.com>
2016-05-19 08:57:47 +0200

    First commit

---
+++ testy/SJF2Test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Wojciech Okoński
+ *         created on 09.03.2016 r.
+ */
+public class SJF2Test {
+
+    @Test
+    public void testSimulate() throws Exception {
+        assertThat(new SJF2(TestsSets.testSet6()).simulate(), is(6.5));
+    }
+}
